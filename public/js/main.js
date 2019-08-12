3ffc3a8fdc40750b50745b011745dae346f1044a
Ryan White <mrwhiterk@gmail.com>
2019-08-12 18:02:11 -0400

    pushed a working version; commented out ajax

--- public/js/main.js
+++ public/js/main.js
@@ -6,21 +6,19 @@ let todoBodies = document.querySelectorAll('.todo');
 });
 
 function editTextMode(e) {
-  let todoText = e.target.innerText;
-  let id = e.target.id;
-  e.target.innerHTML = `<input id='editForm' type='text' value='${todoText}' />`;
-
-  let url = '/updateText/1';
-
-  let editForm = document.querySelector('#editForm');
-  editForm.addEventListener('blur', e => {
-    fetch(url, {
-      id,
-      method: 'POST',
-      body: e.target.value
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
-  });
+  // let todoText = e.target.innerText;
+  // let id = e.target.id;
+  // e.target.innerHTML = `<input id='editForm' type='text' value='${todoText}' />`;
+  // let url = '/updateText/1';
+  // let editForm = document.querySelector('#editForm');
+  // editForm.addEventListener('blur', e => {
+  //   fetch(url, {
+  //     id,
+  //     method: 'POST',
+  //     body: e.target.value
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  //     .catch(err => console.log(err));
+  // });
 }
